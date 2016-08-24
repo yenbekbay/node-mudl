1083decb07baa5760954414f1fc5317efb6a113c
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-08-24 13:38:25 +0600

    feat(parseQuery): improve accuracy

--- src/parseQuery.js
+++ src/parseQuery.js
@@ -15,20 +15,33 @@ export type TrackQuery = {
 };
 
 export default (...args: Array<string>): ?TrackQuery => {
-  const components = he
+  let components = he
     .decode(args.join(' - '))
-    .replace(/\//g, ' & ')
-    .replace(/\s+[Ff]((eat|t)\.?|eaturing)\s+/g, ' feat. ')
-    .replace(/\s+[Vv](s\.?|ersus)\s+/g, ' vs. ')
-    .replace(/\s+[Dd]j\s+/g, ' DJ ')
-    .replace(/\[/g, '(')
-    .replace(/\]/g, ')')
-    .replace(/\(/g, ' (')
     .replace(/\s+(‒|–|—)\s+/g, ' - ')
     .split(' - ')
     .map(_.trim);
 
   if (components.length < 2) return null;
+
+  components = [components[0], _.drop(components).join(' - ')];
+
+  // Capitalize each word in title if query doesn't contain any foreign chars
+  if (!/[^\x00-\x7F]+/.test(components.join(' - '))) {
+    components = [
+      components[0],
+      components[1].replace(/\b\w/g, (char) => char.toUpperCase())
+    ];
+  }
+
+  components = components.map((comp) => comp
+    .replace(/\//g, ' & ')
+    .replace(/(\s+|\()f((eat|t)\.?|eaturing)\s+/ig, '$1feat. ')
+    .replace(/\s+v(s\.?|ersus)\s+/ig, ' vs. ')
+    .replace(/\s+dj\s+/ig, ' DJ ')
+    .replace(/\[/g, '(')
+    .replace(/\]/g, ')')
+    .replace(/\(/g, ' (')
+  );
 
   const getComponent = (idx: number) => components[idx]
     .replace(featuringArtistRegex, '')
@@ -44,7 +57,12 @@ export default (...args: Array<string>): ?TrackQuery => {
       ? ` feat. ${featuringArtists.join(' & ')}`.replace(/[Aa]nd/g, '&')
       : ''
   );
-  const title = getComponent(1);
+  const title = getComponent(1)
+    .replace(
+      /\([^\(]*(download|preview|out now|premiere|recordings)[^\)]*\)/ig,
+      ''
+    )
+    .trim();
   const version = _.nth(title.match(/((?:\([^\(]*\)\s?)+$)/), 1);
 
   return { primaryArtist, artist, title, version };
